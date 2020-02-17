9166d92bb300b9215a63942fd5274dcfbc7c1130
Cassie Achzenick <caachzenick@gmail.com>
2020-02-17 14:17:04 -0700

    Add update plant feature test

---
+++ spec/features/plants/plant_crud_functionality_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe "A visitor can use all crud functionality on the plants DB" do
+  it "creates a new plant" do
+
+    visit '/plants'
+
+    expect(page).to have_link("Add Plant")
+
+    click_link("Add Plant")
+
+    expect(current_path).to eq("/plants/new")
+
+    fill_in "Species", with: "Rubber Tree"
+
+    click_on "Submit"
+
+    expect(current_path).to eq("/plants")
+
+    expect(page).to have_content("Rubber Tree")
+
+  end
+
+  it "updates a new plant" do
+    plant = Plant.create!(species: "Pothos", name: "Favorite Plant")
+
+    visit "/plants/#{plant.id}"
+
+    click_link("Update #{plant.name}")
+
+    expect(current_path).to eq("/plants/#{plant.id}/edit")
+
+    fill_in "Species", with: "Rubber Tree"
+    fill_in "Name", with: "My Fav Plant"
+    fill_in "Name", with: "https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_rubber-tree_hover_mint_6430e356-673c-4af7-bdaf-3a002800dd1f_1024x1024.jpg?v=1578935840"
+
+    click_on "Submit"
+
+    expect(current_path).to eq("/plants/#{plant.id}")
+
+    expect(page).to have_content("Rubber Tree")
+    expect(page).to have_content("My Fav Plant")
+  end
+end
